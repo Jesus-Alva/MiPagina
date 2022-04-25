d734cdb13d048032235423781c40a26e1fc48602
Jesus-Alva <57468191+Jesus-Alva@users.noreply.github.com>
2022-04-25 14:17:09 -0500

    version 2.0.0

--- enviarCorreo.php
+++ enviarCorreo.php
@@ -6,13 +6,13 @@ $mensaje = $_POST['mensaje'];
 
 $header = 'Enviado desde Mi WEB';
 
-$mensaje = "Mnesaje enviado por: " . $nombre . "\r\n";
-$mensaje .= "Su e-mail es: " . $mail . "\r\n";
-$mensaje .= "Telefono de contacto: " . $telefono . "\r\n";
+$mensaje = "Mnesaje enviado por: " . $_POST['nombre'] . "\r\n";
+$mensaje .= "Su e-mail es: " . $_POST['mail'] . "\r\n";
+$mensaje .= "Telefono de contacto: " . $_POST['tel'] . "\r\n";
 $mensaje .= "Mensaje: " . $_POST['mensaje'] . "\r\n";
 
 $para = 'jesusalvabustamante@gmail.com';
-$asunto = 'Consula de ' . $nombre ;
+$asunto = 'Consulta de ' . $_POST['nombre'] ;
 
 mail($para , $asunto, $mensaje, $header);
 
